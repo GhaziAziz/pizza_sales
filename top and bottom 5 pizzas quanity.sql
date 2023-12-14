7cecf82693554331cc7442764de6d9339144ccde
GHAZI AZIZ <114099662+yruykruyfku@users.noreply.github.com>
2023-12-14 22:18:48 +0500

    Create top and bottom 5 pizzas quanity.sql

---
+++ top and bottom 5 pizzas quanity.sql
@@ -0,0 +1,13 @@
+--Top
+
+SELECT Top 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+
+--Bottom  
+
+SELECT TOP 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold ASC
